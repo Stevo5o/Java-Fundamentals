0f5a17094157dd84fac75ec03359ba4974f29a53
Stephen J. O'Connor <stevo.joc@gmail.com>
2015-01-02 21:17:04 +0000

    timeTest code and time1 comments

--- src/Time1.java
+++ src/Time1.java
@@ -2,12 +2,15 @@
 
 public class Time1 {
    
-   private int hour;
-   private int minute;
-   private int second;
+   private int hour; // 0- 23
+   private int minute; // 0 - 59
+   private int second; // 0 - 59
    
+   // set a new time value using universal time; throw an
+   // exception if the hour, minute or second is invalid
    public void setTime(int hour, int minute, int second) {
       
+      // validate hour, minute and second
       if (hour < 0 || hour >= 24 || minute < 0 || minute >= 60 ||
               second < 0 || second >= 60) {
          throw new IllegalArgumentException(
@@ -19,7 +22,14 @@ public class Time1 {
       this.second = second;
    }
 
+   // convert String in universal-time format (HH:MM:SS)
    public String toUniversalString() {
+      return String.format("%02d:%02d:%02d,", hour, minute, second);
+            
+   }
+   
+   // convert String in standard-time format (H:MM:SS AM or PM)
+   public String toString() {
       return String.format("%d:%02d:%02d %s,",
               ((hour == 0 || hour == 12) ? 12 : hour % 12),
               minute, second, (hour < 12 ? "AM" : "PM"));
