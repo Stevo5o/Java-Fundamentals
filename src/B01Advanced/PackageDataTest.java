cd54a0712994a7061da376ea32b6551d173d4748
Stephen <Stephen@O-Connor-ToshL>
2015-01-05 15:54:30 +0000

    Package-access members of a class

---
+++ src/B01Advanced/PackageDataTest.java
@@ -0,0 +1,46 @@
+package B01Advanced;
+
+/**
+ * @author Stephen Package-access members of a class are accessible by other
+ * classes in the same package
+ */
+
+public class PackageDataTest
+{
+
+   public static void main(String[] args)
+   {
+      PackageData packageData = new PackageData();
+
+      // output String representation of packageData
+      System.out.printf("After instaniation:%n%s%n", packageData);
+
+      // change package access data in packageData object
+      packageData.number = 77;
+      packageData.string = "Goodbye";
+
+      // output String representation of packageData
+      System.out.printf("%nAfter changing values:%n%s%n", packageData);
+   }
+}
+
+// class with package access instance variables
+class PackageData
+{
+
+   int number; // package-access instance variable
+   String string; // package-access instance variable
+
+   // constructor
+   public PackageData()
+   {
+      number = 0;
+      string = "Hello";
+   }
+
+   // return Packagedata object String representation
+   public String toString()
+   {
+      return String.format("number: %d; string: %s", number, string);
+   }
+}
